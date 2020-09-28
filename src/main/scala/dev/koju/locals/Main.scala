d3b5f9c98c74ae3e27919b9689bd738d833c3278
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2020-09-28 15:49:27 -0500

    Bootstrap with http4s/http4s.g8

---
+++ src/main/scala/dev/koju/locals/Main.scala
@@ -0,0 +1,8 @@
+package dev.koju.locals
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    Server.stream[IO].compile.drain.as(ExitCode.Success)
+}
