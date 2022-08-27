9ce11ec9ac1e83fd10fa90629e71660349e801db
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-08-27 07:39:03 -0400

    Fix tab navigation in sign in dialog

--- ui/src/nav/SignInDialog.tsx
+++ ui/src/nav/SignInDialog.tsx
@@ -4,36 +4,11 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import { makeStyles } from 'tss-react/mui';
-import Container from '@mui/material/Container';
 import {Dialog, DialogContent, DialogTitle, IconButton, useMediaQuery, useTheme} from "@mui/material";
 import {Close} from "@mui/icons-material";
 import {useForm} from "react-hook-form";
 import {useAuth} from "../auth/Auth";
-
-const useStyles = makeStyles()((theme) => ({
-    paper: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-    close: {
-        padding: '0 10px',
-        textAlign: 'end',
-        lineHeight: '1rem',
-    },
-}));
+import Box from "@mui/material/Box";
 
 type SignInRequest = {
     email: string,
@@ -46,7 +21,6 @@ interface SignInDialogProps {
 }
 
 export default function SignInDialog({open, onClose}: SignInDialogProps) {
-    const { classes } = useStyles();
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const {refreshAuth} = useAuth();
@@ -71,57 +45,59 @@ export default function SignInDialog({open, onClose}: SignInDialogProps) {
             onClose={onClose}
             fullScreen={fullScreen}
         >
-            <DialogTitle className={classes.close}>
+            <DialogTitle sx={{padding: '0 10px', textAlign: 'end', lineHeight: '1rem',}}>
                 <IconButton edge="end" color="inherit" onClick={onClose} aria-label="close">
                     <Close/>
                 </IconButton>
             </DialogTitle>
             <DialogContent>
-                <Container component="main" maxWidth="xs">
-                    <div className={classes.paper}>
-                        <Avatar className={classes.avatar}>
-                            <LockOutlinedIcon/>
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{mt: 1}}>
+                        <TextField
+                            autoFocus
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            autoComplete="email"
+                            {...register('email', {required: true})}
+                            error={!!errors.email}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            {...register('password', {required: true})}
+                            error={!!errors.password}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}
+                        >
                             Sign In
-                        </Typography>
-                        <form className={classes.form} noValidate onSubmit={handleSubmit(onSubmit)}>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                autoComplete="email"
-                                autoFocus
-                                {...register('email', {required: true})}
-                                error={!!errors.email}
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                                {...register('password', {required: true})}
-                                error={!!errors.password}
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={classes.submit}
-                            >
-                                Sign In
-                            </Button>
-                        </form>
-                    </div>
-                </Container>
+                        </Button>
+                    </Box>
+                </Box>
             </DialogContent>
         </Dialog>
     );
