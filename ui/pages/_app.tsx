dd0f7caff98a3187eeeec0472b10023faf8a275b
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2022-09-26 12:00:17 -0400

    Remove unnecessary files from template

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,35 +1,24 @@
 import * as React from 'react';
 import Head from 'next/head';
-import {AppProps} from 'next/app';
-import {ThemeProvider} from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import {CacheProvider, EmotionCache} from '@emotion/react';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
 import {AuthProvider} from "../src/auth/Auth";
 import NavBar from "../src/nav/NavBar";
+import {AppProps} from "next/app";
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
+const theme = createTheme();
 
-interface MyAppProps extends AppProps {
-    emotionCache?: EmotionCache;
-}
-
-export default function MyApp(props: MyAppProps) {
-    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+export default function MyApp({Component, pageProps}: AppProps) {
     return (
-        <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
             <Head>
                 <meta name="viewport" content="initial-scale=1, width=device-width"/>
             </Head>
-            <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <AuthProvider>
-                    <NavBar/>
-                    <Component {...pageProps} />
-                </AuthProvider>
-            </ThemeProvider>
-        </CacheProvider>
+            <CssBaseline/>
+            <AuthProvider>
+                <NavBar/>
+                <Component {...pageProps} />
+            </AuthProvider>
+        </ThemeProvider>
     );
 }
